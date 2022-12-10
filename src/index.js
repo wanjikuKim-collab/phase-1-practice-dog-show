7e9f48809624ceb61e0bdb93621243e654bcd098
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-10 19:28:26 +0300

    Code optimized

--- src/index.js
+++ src/index.js
@@ -13,34 +13,26 @@ function getDogs(){
 const addData= async ()=>{
     let tableBody = document.getElementById('table-body')
     let dogs = await getDogs()
-    console.log(dogs)
-   
+    console.log(dogs) 
+    
     dogs.map(dog=>{
-        //create row element
-        let row = document.createElement('tr');
-
-        //create cells
-        let c1= document.createElement('td')
-        let c2= document.createElement('td')
-        let c3= document.createElement('td')
-        let c4= document.createElement('td')
-
-        //add data to cells
-        c1.textContent=dog.name;
-        c2.textContent=dog.breed;
-        c3.textContent=dog.sex;
-        c4.innerHTML=`<button id="edit">Edit Dog</button>`;
-
-        //append cells to row
-        row.appendChild(c1);
-        row.appendChild(c2);
-        row.appendChild(c3);
-        row.appendChild(c4);
-
-        //append row to table body
-        tableBody.appendChild(row);
+        const row = document.createElement('tr')
+        row.innerHTML= `
+        <tr>
+            <td>${dog.name} </td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td><button>Edit Dog</button></td>
+        </tr>        
+        `
+        tableBody.appendChild(row)
+        
+        const btn = document.querySelector('button');
+        btn.addEventListener('click',editDog)
     })
 }
+
+
 
 
 
