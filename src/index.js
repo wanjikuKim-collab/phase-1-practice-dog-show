d176dd92da21a2e7e713a9aaf71b96362d2820bd
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-10 18:54:51 +0300

    Adding data to Tbody

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    addData()
 })
+
+
+function getDogs(){
+    return fetch("http://localhost:3000/dogs")
+    .then(resp=>resp.json())
+    .then(data=>data)
+}
+
+
+const addData= async ()=>{
+    let tableBody = document.getElementById('table-body')
+    let dogs = await getDogs()
+    console.log(dogs)
+   
+    dogs.map(dog=>{
+        //create row element
+        let row = document.createElement('tr');
+
+        //create cells
+        let c1= document.createElement('td')
+        let c2= document.createElement('td')
+        let c3= document.createElement('td')
+        let c4= document.createElement('td')
+
+        //add data to cells
+        c1.textContent=dog.name;
+        c2.textContent=dog.breed;
+        c3.textContent=dog.sex;
+        c4.innerHTML=`<button id="edit">Edit Dog</button>`;
+
+        //append cells to row
+        row.appendChild(c1);
+        row.appendChild(c2);
+        row.appendChild(c3);
+        row.appendChild(c4);
+
+        //append row to table body
+        tableBody.appendChild(row);
+    })
+}
+
+
+
+
+
+
+
+
+
