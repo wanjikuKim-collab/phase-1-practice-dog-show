3c1ecfb7e39c964fd117975497b8c5c789f666a5
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-10 19:55:48 +0300

    Click Edit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    addData()
+    addData();
 })
 
-
+//fetches the dog data
 function getDogs(){
     return fetch("http://localhost:3000/dogs")
     .then(resp=>resp.json())
@@ -10,14 +10,15 @@ function getDogs(){
 }
 
 
+// adds and displays data to the table body
 const addData= async ()=>{
     let tableBody = document.getElementById('table-body')
     let dogs = await getDogs()
     console.log(dogs) 
     
     dogs.map(dog=>{
-        const row = document.createElement('tr')
-        row.innerHTML= `
+        const tableRow = document.createElement('tr')
+        tableRow.innerHTML= `
         <tr>
             <td>${dog.name} </td>
             <td>${dog.breed}</td>
@@ -25,14 +26,25 @@ const addData= async ()=>{
             <td><button>Edit Dog</button></td>
         </tr>        
         `
-        tableBody.appendChild(row)
-        
-        const btn = document.querySelector('button');
-        btn.addEventListener('click',editDog)
+        tableBody.appendChild(tableRow)       
+
     })
+    tableBody.addEventListener('click',editDog)
 }
 
 
+
+//edits dog information
+const editDog=(e)=>{
+    const form = document.querySelector('form')
+    const btn = document.querySelector('button')
+    let row = btn.parentElement.parentElement.children;
+    console.log(row)
+	form.children[0].value = row[0].innerText
+	form.children[1].value = row[1].innerText
+	form.children[2].value = row[2].innerText
+	form.children[3].dataset.id = row[3].children[0].dataset.id
+}
 
 
 
